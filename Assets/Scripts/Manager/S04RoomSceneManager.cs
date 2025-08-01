2a88a2c6429ff7874430f3ccde835f63bd68edf2
Saptak Manna <saptakmanna100@gmail.com>
2025-08-01 18:46:58 +0530

    Refactor scene management: rename SceneManager to S04RoomSceneManager, implement interaction handling, and remove obsolete SceneManager script

---
+++ Assets/Scripts/Manager/S04RoomSceneManager.cs
@@ -0,0 +1,98 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S04RoomSceneManager : MonoBehaviour
+{
+  public static S04RoomSceneManager Instance { get; private set; }
+
+  [Serializable]
+  struct InteractableObject
+  {
+    public Button button;
+    public DialogueNodeSo dialogueNode;
+  }
+
+  [SerializeField] private InteractableObject bedObject;
+  [SerializeField] private InteractableObject calendarObject;
+  [SerializeField] private InteractableObject doorObject;
+  [SerializeField] private InteractableObject tvObject;
+  [SerializeField] private InteractableObject musicPlayerObject;
+  [SerializeField] private InteractableObject booksObject;
+
+  private void Awake()
+  {
+    Instance = this;
+  }
+
+  private void Start()
+  {
+    bedObject.button.onClick.AddListener(() => HandleBedInteraction());
+    calendarObject.button.onClick.AddListener(() => HandleCalendarInteraction());
+    doorObject.button.onClick.AddListener(() => HandleDoorInteraction());
+    tvObject.button.onClick.AddListener(() => HandleTvInteraction());
+    musicPlayerObject.button.onClick.AddListener(() => HandleMusicPlayerInteraction());
+    booksObject.button.onClick.AddListener(() => HandleBooksInteraction());
+  }
+
+  private void HandleBedInteraction()
+  {
+    GameManager.Instance.TriggerDialogue(bedObject.dialogueNode);
+  }
+
+  private void HandleCalendarInteraction()
+  {
+    GameManager.Instance.TriggerDialogue(calendarObject.dialogueNode);
+
+    EventHandler onDialogueEndHandler = null;
+    onDialogueEndHandler = (sender, e) =>
+    {
+      DialogueManager.Instance.OnDialogueEnd -= onDialogueEndHandler;
+
+      if (GameManager.Instance.gameState.CalViewed is 0 or 2 or 4)
+      {
+        GameManager.Instance.gameState.CalViewed++;
+      }
+    };
+    DialogueManager.Instance.OnDialogueEnd += onDialogueEndHandler;
+  }
+
+  private void HandleTvInteraction()
+  {
+    GameManager.Instance.TriggerDialogue(tvObject.dialogueNode);
+
+    EventHandler onDialogueEndHandler = null;
+    onDialogueEndHandler = (sender, e) =>
+    {
+      DialogueManager.Instance.OnDialogueEnd -= onDialogueEndHandler;
+
+      if (GameManager.Instance.gameState.TvViewed is 0 or 2)
+      {
+        GameManager.Instance.gameState.TvViewed++;
+      }
+    };
+    DialogueManager.Instance.OnDialogueEnd += onDialogueEndHandler;
+  }
+
+  private void HandleDoorInteraction()
+  {
+    if (GameManager.Instance.gameState.FreeRoam)
+    {
+      // If free roam is enabled, we can open the door
+    }
+    else
+    {
+      GameManager.Instance.TriggerDialogue(doorObject.dialogueNode);
+    }
+  }
+
+  private void HandleMusicPlayerInteraction()
+  {
+    GameManager.Instance.TriggerDialogue(musicPlayerObject.dialogueNode);
+  }
+
+  private void HandleBooksInteraction()
+  {
+    GameManager.Instance.TriggerDialogue(booksObject.dialogueNode);
+  }
+}
