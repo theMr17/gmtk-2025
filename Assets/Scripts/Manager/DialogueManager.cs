db2029c278ecc110b08020ba0c7210e52bfff6a8
Saptak Manna <saptakmanna100@gmail.com>
2025-08-03 21:34:04 +0530

    Update GameEndDialogue text and enhance DialogueManager to play sound on dialogue advancement

--- Assets/Scripts/Manager/DialogueManager.cs
+++ Assets/Scripts/Manager/DialogueManager.cs
@@ -162,6 +162,8 @@ public class DialogueManager : MonoBehaviour
         interactionButtonsContainer.SetActive(false); // Hide interaction buttons at start
     }
 
+    SoundManager.PlaySound(SoundType.DialogueAdvance);
+
     if (currentLine.action != DialogueActionType.None)
     {
       TriggerDialogueAction(currentLine.action);
